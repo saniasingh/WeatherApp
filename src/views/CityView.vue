e67660cd7b2dbb3aacd2d29d688c853c53f3d963
Sania Singh <saniasingh4in@gmail.com>
2024-01-28 01:49:24 +0530

    first commit

---
+++ src/views/CityView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Suspense>
+      <AsyncCityview />
+      <template #fallback>
+        <CityViewSkelton />
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script setup>
+import AsyncCityview from "../components/AsyncCityview.vue";
+import CityViewSkelton from "@/components/CityViewSkelton.vue";
+</script>
